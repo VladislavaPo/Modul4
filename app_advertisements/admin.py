e4709c5df9a71ba84f7bc66f38f9bddb899b3917
Влада <ladaumkina@gmail.com>
2023-10-08 22:03:22 +0300

    Модуль 4 Урок 4

--- app_advertisements/admin.py
+++ app_advertisements/admin.py
@@ -1,3 +1,29 @@
-from django.contrib import admin
+from django.contrib import admin  # работает с панелью админа\
+from .models import Advertisement  # для получения данных из бд
 
-# Register your models here.
+
+class AdvertisementAdmin(admin.ModelAdmin):  # наследуем из род. класса работающего с админской панелью для переопределения структур
+    # pass  # для тестирования
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'auction']  # то что будет отображаться
+    list_filter = ['auction', 'created_at']  # фильтрация по
+    actions = ['make_auction_as_false', 'make_auction_as_true']  # какие-то быстрые действия
+    fieldsets = (
+        ('Общее', {
+            'fields': ('title', 'description')
+        }),
+        ('Финансы', {
+            'fields': ('price', 'auction'),
+            'classes':  ['collapse']  # для отработки навыков сделаем возможным скрывать и раскрывать вкладку "Финансы"
+        })
+    )  # группировка полей на отдельные смысловые блоки
+
+    @admin.action(description="Убрать возможность торга")  # декоратор
+    def make_auction_as_false(self, request, queryset):  # чтобы указать каким объектом будем применять действия и что и как будем менять
+        queryset.update(auction=False)  # чтобы указать каким объектом будем применять действия (название поля = чему будет равняться)
+
+    @admin.action(description="Добавить возможность торга")
+    def make_auction_as_true(self, request, queryset):
+        queryset.update(auction=True)
+
+
+admin.site.register(Advertisement, AdvertisementAdmin)  # регестрируем модель
--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -1,14 +1,26 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
 
 
 # класс для объявлений
 class Advertisement(models.Model):  # для создания модели из класса нужно применить способ наследования - models.Model - превращаем класс в модель, а позже в таблипцу
     title = models.CharField("Заголовок", max_length= 128)  # (порядок: "альтернативное название", максимальное количество символос(принято обозначать в степени двойки))
-    description = models.TextField("Название")  # описание("альтернативное название")
+    description = models.TextField("Описание")  # описание("альтернативное название")
     price = models.DecimalField("Цена", max_digits=10, decimal_places=2)  # цена("альтернативное название", максимальное количество цифр всего и перед и за точкой, сколько знаков после точки)
     auction = models.BooleanField("Торг", help_text="Отметьте, если торг уместен")  # может ли быть скидка("альтернативное название", помощь/доп текст выведется при наведении)
     created_at = models.DateTimeField(auto_now_add=True)  # дата создания(для автоматической записи времени один раз)
     updated_at = models.DateTimeField(auto_now=True)  # дата редактирования(автоматическая запись времени обновляющаяся при каждом изменении)
+
+    @admin.display(description='Дата создания')
+    def created_date(self):  # для того чтобы если время заявление создано сегодня то выводилось "Сегодня в ***"
+        from django.utils import timezone  # библиотека работает только в этой функции
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')  # формат времени
+            return format_html(
+                '<span style="color: green; font-weight:bold;">Сегодня в {}</span>', created_time
+            )  # отдельный стиль
+        return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')  # если дата другая
 
     class Meta:
         db_table = "advertisements"  # поле для изменения названия таблицы
