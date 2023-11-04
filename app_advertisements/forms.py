9cfdb51f6dd76455dabfbe2fc9ac15d8fcc44f07
Влада <ladaumkina@gmail.com>
2023-11-04 21:27:17 +0300

    Модуль 4 Урок 6 дз №4.1

--- app_advertisements/forms.py
+++ app_advertisements/forms.py
@@ -1,9 +1,16 @@
 from django import forms
+from .models import Advertisement
 
 
-class AdvertisementForm(forms.Form):  # форма для создания
-    title = forms.CharField(max_length=64, widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}))
-    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control form-control-lg'}))
-    price = forms.DecimalField(widget=forms.NumberInput(attrs={'class': 'form-control form-control-lg'}))
-    auction = forms.BooleanField(widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}), required=False)
-    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control form-control-lg'}))
+class AdvertisementForm(forms.ModelForm):  # дз
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['description'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['image'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['price'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['auction'].widget.attrs['class'] = 'form-check-input'
+
+    class Meta:
+        model = Advertisement
+        fields = ("title", "description", "image", "price", "auction")
